6f7651e6cc23f62073036fa5ec744f1a133b7f2d
cymdaspec <1337xm@gmail.com>
2021-11-09 03:05:31 +0300

    123

---
+++ HW5/p1ex1.dart
@@ -0,0 +1,31 @@
+class Collections
+{
+  static int TimeCount = 0;
+  static int sum(List <int> list)
+  {
+    TimeCount++;
+    int Result = 0;
+    for (int i = 0; i < list.length; i++)
+      {
+        Result += list[i];
+      }
+    return Result;
+
+  }
+
+  static int time ()
+  {
+    return TimeCount;
+  }
+
+}
+
+
+void main()
+{
+  print(Collections.time); //0
+  print(Collections.sum([1, 2, 3, 5])); //11
+  print(Collections.time); //1
+  print(Collections.sum([6, 2, 5])); //13
+  print(Collections.time); //2
+}
